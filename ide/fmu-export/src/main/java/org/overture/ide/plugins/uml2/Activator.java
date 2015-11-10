0a8f64ec91bd4c9d1e285c90c155e684543f642e
Kenneth Lausdahl <lausdahl@eng.au.dk>
2015-11-10 16:31:25 +0100

    change repo structure and enabled the ability to build eclipse plugins

---
+++ ide/fmu-export/src/main/java/org/overture/ide/plugins/uml2/Activator.java
@@ -0,0 +1,97 @@
+/*
+ * #%~
+ * UML2 Translator
+ * %%
+ * Copyright (C) 2008 - 2014 Overture
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #~%
+ */
+package org.overture.ide.plugins.uml2;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends AbstractUIPlugin
+{
+
+	// The plug-in ID
+	//public static final String PLUGIN_ID = IUml2Constants.PLUGIN_ID;
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator()
+	{
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception
+	{
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception
+	{
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault()
+	{
+		return plugin;
+	}
+
+	public static void log(Exception exception)
+	{
+	//	getDefault().getLog().log(new Status(IStatus.ERROR, IUml2Constants.PLUGIN_ID, "UMLPlugin", exception));
+	}
+
+	public static void log(String message, Exception exception)
+	{
+		//getDefault().getLog().log(new Status(IStatus.ERROR, IUml2Constants.PLUGIN_ID, message, exception));
+	}
+
+	/**
+	 * Initializes a preference store with default preference values for this plug-in.
+	 */
+	@Override
+	protected void initializeDefaultPreferences(IPreferenceStore store)
+	{
+		//store.setDefault(IUml2Constants.PREFER_ASSOCIATIONS_PREFERENCE, true);
+		//store.setDefault(IUml2Constants.DISABLE_NESTED_ARTIFACTS_PREFERENCE, true);
+	}
+}
