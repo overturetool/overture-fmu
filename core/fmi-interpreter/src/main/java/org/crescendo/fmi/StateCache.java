7cb1982fd768f1cc1f291662b7f5e075c3dc0c52
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-04-28 12:53:50 +0200

    fixed #4 now the max value reference is calculated based on the max per type from the model description file

--- core/fmi-interpreter/src/main/java/org/crescendo/fmi/StateCache.java
+++ core/fmi-interpreter/src/main/java/org/crescendo/fmi/StateCache.java
@@ -31,92 +31,144 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class StateCache {
-	
+public class StateCache
+{
+
 	final static Logger logger = LoggerFactory.getLogger(StateCache.class);
 
-	final int size = 20;
-	public final double[] reals = new double[size];
-	public final int[] integers = new int[size];
-	public final boolean[] booleans = new boolean[size];
+	public final double[] reals;
+	public final int[] integers;
+	public final boolean[] booleans;
 
 	public final Links links;
 
-	public StateCache(File linkFile)
-			throws XPathExpressionException, DOMException, SAXException, IOException, ParserConfigurationException {
+	public StateCache(File linkFile) throws XPathExpressionException,
+			DOMException, SAXException, IOException,
+			ParserConfigurationException
+	{
 		links = createVdmLinks(linkFile);
+
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		Document doc = docBuilderFactory.newDocumentBuilder().parse(linkFile);
+
+		reals = new double[getMaxInt(doc, "//ScalarVariable[Real]/@valueReference") + 1];
+		integers = new int[getMaxInt(doc, "//ScalarVariable[Integer]/@valueReference") + 1];
+		booleans = new boolean[getMaxInt(doc, "//ScalarVariable[Boolean]/@valueReference") + 1];
 	}
 
-	public List<StepinputsStructParam> collectInputsFromCache() {
+	public List<StepinputsStructParam> collectInputsFromCache()
+	{
 		List<StepinputsStructParam> inputs = new Vector<StepinputsStructParam>();
 
-		for (Entry<String, LinkInfo> entry : links.getInputs().entrySet()) {
+		for (Entry<String, LinkInfo> entry : links.getInputs().entrySet())
+		{
 
 			Double value = 0.0;
-			switch (((ExtendedLinkInfo) entry.getValue()).type) {
-			case Boolean:
-				value = booleans[Integer.valueOf(entry.getKey())] ? 1.0 : 0.0;
-				break;
-			case Integer:
-				value = new Double(integers[Integer.valueOf(entry.getKey())]);
-				break;
-			case Real:
-				value = reals[Integer.valueOf(entry.getKey())];
-				break;
-			case String:
-				break;
-			default:
-				break;
+			switch (((ExtendedLinkInfo) entry.getValue()).type)
+			{
+				case Boolean:
+					value = booleans[Integer.valueOf(entry.getKey())] ? 1.0
+							: 0.0;
+					break;
+				case Integer:
+					value = new Double(integers[Integer.valueOf(entry.getKey())]);
+					break;
+				case Real:
+					value = reals[Integer.valueOf(entry.getKey())];
+					break;
+				case String:
+					break;
+				default:
+					break;
 
 			}
 
-			inputs.add(new StepinputsStructParam(entry.getKey(), Arrays.asList(new Double[] { value }),
-					Arrays.asList(new Integer[] { 1 })));
-			logger.debug("Collecting inputs from cache name: '{}' value: '{}' size: '{}' valueref: '{}'",links.getBoundVariableInfo(entry.getKey()).getQualifiedNameString(),value,1,entry.getKey());
+			inputs.add(new StepinputsStructParam(entry.getKey(), Arrays.asList(new Double[] { value }), Arrays.asList(new Integer[] { 1 })));
+			logger.debug("Collecting inputs from cache name: '{}' value: '{}' size: '{}' valueref: '{}'", links.getBoundVariableInfo(entry.getKey()).getQualifiedNameString(), value, 1, entry.getKey());
 		}
 
 		return inputs;
 	}
 
-	public void syncOutputsToCache(List<StepStructoutputsStruct> outputs) {
-		for (StepStructoutputsStruct output : outputs) {
+	public void syncOutputsToCache(List<StepStructoutputsStruct> outputs)
+	{
+		for (StepStructoutputsStruct output : outputs)
+		{
 			// System.out.println(String.format("\tOutput %s = %s", output.name,
 			// output.value.get(0)));
 			ExtendedLinkInfo link = (ExtendedLinkInfo) links.getLinks().get(output.name);
-			
+
 			Object vdmName = link.getQualifiedNameString();
 
 			int index = Integer.valueOf(output.name);
-			switch (link.type) {
-			case Boolean:
-				booleans[index] = output.value.get(0) > 0;
-				logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'",vdmName,booleans[index],index);
-				break;
-			case Integer:
-				integers[index] = output.value.get(0).intValue();
-				logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'",vdmName,integers[index],index);
-				break;
-			case Real:
-				reals[index] = output.value.get(0);
-				logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'",vdmName,reals[index],index);
-				break;
-			case String:
-				
-				//logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'",vdmName,strings[index],index);
-				break;
-			default:
-				break;
+			switch (link.type)
+			{
+				case Boolean:
+					booleans[index] = output.value.get(0) > 0;
+					logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'", vdmName, booleans[index], index);
+					break;
+				case Integer:
+					integers[index] = output.value.get(0).intValue();
+					logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'", vdmName, integers[index], index);
+					break;
+				case Real:
+					reals[index] = output.value.get(0);
+					logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'", vdmName, reals[index], index);
+					break;
+				case String:
+
+					// logger.debug("Sync output to fmi struct name: '{}' value: '{}' valueref: '{}'",vdmName,strings[index],index);
+					break;
+				default:
+					break;
 
 			}
-			
-			
 
 		}
 
 	}
 
-	static Links createVdmLinks(File linkFile)
-			throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, DOMException {
+	/**
+	 * Utility method to calculate largest integer represented by the nodes obtained by the xpathquery
+	 * 
+	 * @param doc
+	 * @param xpathQuery
+	 * @return
+	 * @throws XPathExpressionException
+	 * @throws DOMException
+	 */
+	static int getMaxInt(Document doc, String xpathQuery)
+			throws XPathExpressionException, DOMException
+	{
+		XPathFactory xPathfactory = XPathFactory.newInstance();
+		XPath xpath = xPathfactory.newXPath();
+
+		int max = 0;
+
+		for (Node n : new NodeIterator(lookup(doc, xpath, xpathQuery)))
+		{
+
+			String val = n.getNodeValue();
+			try
+			{
+				int nv = Integer.parseInt(val);
+				if (nv > max)
+				{
+					max = nv;
+				}
+
+			} catch (NumberFormatException e)
+			{
+			}
+
+		}
+		return max;
+	}
+
+	static Links createVdmLinks(File linkFile) throws SAXException,
+			IOException, ParserConfigurationException,
+			XPathExpressionException, DOMException
+	{
 		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 		Document doc = docBuilderFactory.newDocumentBuilder().parse(linkFile);
 
@@ -128,19 +180,23 @@ public class StateCache {
 		final List<String> inputs = new Vector<String>();
 		final List<String> designParameters = new Vector<String>();
 
-		for (Node n : new NodeIterator(lookup(doc, xpath, "//ScalarVariable"))) {
+		for (Node n : new NodeIterator(lookup(doc, xpath, "//ScalarVariable")))
+		{
 			NamedNodeMap attributes = n.getAttributes();
 			String valRef = attributes.getNamedItem("valueReference").getNodeValue();
 
 			String name = null;
 
-			NodeList nameNodes = lookup(doc, xpath, "/fmiModelDescription/VendorAnnotations/Tool[@name='Overture']/Overture/link[@valueReference='" + valRef + "']/@name");
-			if (nameNodes != null && nameNodes.getLength() > 0) {
+			NodeList nameNodes = lookup(doc, xpath, "/fmiModelDescription/VendorAnnotations/Tool[@name='Overture']/Overture/link[@valueReference='"
+					+ valRef + "']/@name");
+			if (nameNodes != null && nameNodes.getLength() > 0)
+			{
 
 				name = nameNodes.item(0).getNodeValue();
 			}
 
-			if (name == null) {
+			if (name == null)
+			{
 				name = attributes.getNamedItem("name").getNodeValue();
 			}
 
@@ -148,17 +204,20 @@ public class StateCache {
 			ExtendedLinkInfo.Type type = ExtendedLinkInfo.Type.Real;
 
 			for (@SuppressWarnings("unused")
-			Node n1 : new NodeIterator(lookup(n, xpath, "Real"))) {
+			Node n1 : new NodeIterator(lookup(n, xpath, "Real")))
+			{
 				type = ExtendedLinkInfo.Type.Real;
 			}
 
 			for (@SuppressWarnings("unused")
-			Node n1 : new NodeIterator(lookup(n, xpath, "Boolean"))) {
+			Node n1 : new NodeIterator(lookup(n, xpath, "Boolean")))
+			{
 				type = ExtendedLinkInfo.Type.Boolean;
 			}
 
 			for (@SuppressWarnings("unused")
-			Node n1 : new NodeIterator(lookup(n, xpath, "Integer"))) {
+			Node n1 : new NodeIterator(lookup(n, xpath, "Integer")))
+			{
 				type = ExtendedLinkInfo.Type.Integer;
 			}
 
@@ -166,11 +225,14 @@ public class StateCache {
 
 			String causality = attributes.getNamedItem("causality").getNodeValue();
 
-			if ("output".equals(causality)) {
+			if ("output".equals(causality))
+			{
 				outputs.add(valRef);
-			} else if ("input".equals(causality)) {
+			} else if ("input".equals(causality))
+			{
 				inputs.add(valRef);
-			} else if ("parameter".equals(causality)) {
+			} else if ("parameter".equals(causality))
+			{
 				designParameters.add(valRef);
 			}
 
@@ -181,28 +243,36 @@ public class StateCache {
 
 	final static boolean DEBUG = false;
 
-	static NodeList lookup(Object doc, XPath xpath, String expression) throws XPathExpressionException {
+	static NodeList lookup(Object doc, XPath xpath, String expression)
+			throws XPathExpressionException
+	{
 		XPathExpression expr = xpath.compile(expression);
 
-		if (DEBUG) {
+		if (DEBUG)
+		{
 			// System.out.println("Starting from: " + formateNodeWithAtt(doc));
 		}
 		final NodeList list = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
 
-		if (DEBUG) {
+		if (DEBUG)
+		{
 			System.out.print("\tFound: ");
 		}
 		boolean first = true;
 		for (@SuppressWarnings("unused")
-		Node n : new NodeIterator(list)) {
-			if (DEBUG) {
+		Node n : new NodeIterator(list))
+		{
+			if (DEBUG)
+			{
 				// System.out.println((!first ? "\t " : "")
 				// + formateNodeWithAtt(n));
 			}
 			first = false;
 		}
-		if (first) {
-			if (DEBUG) {
+		if (first)
+		{
+			if (DEBUG)
+			{
 				System.out.println("none");
 			}
 		}
