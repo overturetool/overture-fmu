a0c12c2fe494159d7baf48c1fabb2ab62e70eed7
Victor Bandur <vpb500@york.ac.uk>
2017-03-21 12:01:27 +0100

    WIP failure when calling CG.

--- core/fmu-import-export/src/main/java/org/overturetool/fmi/export/CGenerator.java
+++ core/fmu-import-export/src/main/java/org/overturetool/fmi/export/CGenerator.java
@@ -41,13 +41,14 @@ public class CGenerator
 		vdm2c.getCGenSettings().setUseGarbageCollection(true);
 				
 		try {
+			out.println("generating");
 			GeneratedData data = vdm2c.generate(nodes);
+			out.println("finished generating");
 			vdm2c.genCSourceFiles(outputDir, data.getClasses());
 			vdm2c.emitFeatureFile(outputDir,  CGen.FEATURE_FILE_NAME);
 		}catch (AnalysisException e) {
 			out.println("Analysis exception");
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
