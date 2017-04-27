b3bea860005265b5336a8727073f59436203836f
Victor Bandur <vpb500@york.ac.uk>
2017-04-27 16:44:39 +0200

    -  Removing stale definition of unpackString.

--- core/fmu-import-export/src/main/resources/c-templates/FmuIO.c
+++ core/fmu-import-export/src/main/resources/c-templates/FmuIO.c
@@ -17,27 +17,6 @@
 #include "PrettyPrint.h"
 
 
-char* unpackString(TVP charseq)
-{
-	int i;
-	char* str;
-
-
-
-	UNWRAP_COLLECTION(col, charseq);
-
-	str = (char*)malloc(col->size * sizeof(char) + 1);
-	str[col->size] = 0;
-
-	for(i = 0; i < col->size; i++)
-	{
-		ASSERT_CHECK_CHAR((col->value[i]));
-		str[i] = ((col->value[i])->value).charVal;
-	}
-
-	return str;
-}
-
 TVP vdm_IO_freadval(TVP filename)
 {
 	/*  TODO.  */
