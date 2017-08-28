0c785bde3401cfb9ea04a889c805da62b12e3ec5
Victor Bandur <victor.bandur@eng.au.dk>
2017-08-28 11:12:24 +0200

    -  Initial integration with FMU checker. -  Addresses #59.

--- testing/validate.sh
+++ testing/validate.sh
@@ -32,6 +32,84 @@ echo "Test will be performed on: '${JAR}'"
 echo "Test will be performed on model: ${MODEL}"
 
 rm -rf output
+rm -rf output-source 
+
 java -jar $JAR -export tool -name $NAME -root $MODEL -output output
+java -jar $JAR -export source -name $NAME -root $MODEL -output output-source
+
+
+# Validate tool wrapper FMU.
 
 $FMU_CHECK_DIR/fmuCheck*  -h 0.001 -s 30  output/wt2.fmu
+rm -rf output
+
+# Validate source code FMU.
+## Create source code FMU.
+cd output-source
+
+unzip $NAME.fmu
+
+cp ../CMakeLists.txt .
+sed -i "s/##NAME##/$NAME/g" CMakeLists.txt
+
+## Read defines if any
+if [ -e "sources/defines.def" ] 
+then
+
+defs=""
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+  defs="$defs -D$line"
+done < "sources/defines.def"
+
+defs="add_definitions(${defs})"
+
+sed -i "s/##DEFINITIONS##/${defs}/g" CMakeLists.txt
+
+fi
+
+
+## Read additional includes if any
+if [ -e "sources/includes.txt" ] 
+then
+
+includes=""
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+  includes="$includes sources/$line"
+done < "sources/includes.txt"
+
+
+includes="include_directories(${includes})"
+echo "additional includes ${includes}"
+sed -i "s|##INCLUDES##|${includes}|g" CMakeLists.txt
+
+fi
+
+
+## Compile source code FMU.
+cmake .
+make -j5
+mkdir -p binaries/linux64
+cp wt2.so binaries/linux64
+
+##  Add library to source code FMU.
+zip -ur wt2.fmu binaries/
+cd ..
+
+
+## Test source code FMU.
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS" ;;
+  darwin*)  echo "OSX"; LIB=`readlink -f output-source/binaries/darwin64/$NAME.dylib`;; 
+  linux*)   echo "LINUX"; LIB=`readlink -f output-source/binaries/linux64/$NAME.so`;; 
+  bsd*)     echo "BSD" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
+
+$FMU_CHECK_DIR/fmuCheck*  -h 0.001 -s 30  output-source/wt2.fmu
+
+echo "Clean up."
+rm -rf output-source
